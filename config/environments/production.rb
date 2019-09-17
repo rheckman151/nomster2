fb3931279381cd14a2f83401f40d2a40f3342c75
rheckman151 <frisbeeryan151@gmail.com>
2019-09-17 05:05:30 +0000

    devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'nomster-ryan-heckman.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
