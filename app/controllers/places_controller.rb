7df002dd65518366d3c58dec414adc13b8b99b40
rheckman151 <frisbeeryan151@gmail.com>
2019-09-17 05:58:37 +0000

    figaro

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     def index
         @places = Place.paginate(:page => params[:page], :per_page => 2)
     end
@@ -9,9 +10,13 @@ class PlacesController < ApplicationController
     end
 
     def create
-        current_user.places.create(place_params)
-        redirect_to root_path
-    end
+        @place = current_user.places.create(place_params)
+        if @place.valid?
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
+      end
 
     def show
         @place = Place.find(params[:id])
@@ -19,19 +24,36 @@ class PlacesController < ApplicationController
     
     def edit
         @place = Place.find(params[:id])
+
+        if @place.user != current_user
+            return render plain: 'Not Allowed', status: :forbidden
+        end
     end
 
     def update
         @place = Place.find(params[:id])
+      
+        if @place.user != current_user
+          return render plain: 'Not Allowed', status: :forbidden
+        end
+      
         @place.update_attributes(place_params)
-        redirect_to root_path
-    end
+        if @place.valid?
+          redirect_to root_path
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
 
-    def destroy
+      def destroy
         @place = Place.find(params[:id])
+        if @place.user != current_user
+          return render plain: 'Not Allowed', status: :forbidden
+        end
+      
         @place.destroy
         redirect_to root_path
-    end
+      end
 
     private
     
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,9 @@
 class Place < ApplicationRecord
     belongs_to :user
+    geocoded_by :address
+    after_validation :geocode
+    
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :name, presence: true, length: { minimum: 3 }
 end
