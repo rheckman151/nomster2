949148b366cc703ac277c18e67477e082fb4b500
rheckman151 <frisbeeryan151@gmail.com>
2019-09-17 05:35:42 +0000

    edit, destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,7 +12,27 @@ class PlacesController < ApplicationController
         current_user.places.create(place_params)
         redirect_to root_path
     end
+
+    def show
+        @place = Place.find(params[:id])
+    end
     
+    def edit
+        @place = Place.find(params[:id])
+    end
+
+    def update
+        @place = Place.find(params[:id])
+        @place.update_attributes(place_params)
+        redirect_to root_path
+    end
+
+    def destroy
+        @place = Place.find(params[:id])
+        @place.destroy
+        redirect_to root_path
+    end
+
     private
     
     def place_params
